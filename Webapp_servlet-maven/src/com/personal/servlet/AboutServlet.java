a456126c608f6f135993a8857e0574a027b0a91a
edward <edward.schudel@yahoo.fr>
2018-10-18 10:03:50 +0200

    first structure

---
+++ Webapp_servlet-maven/src/com/personal/servlet/AboutServlet.java
@@ -0,0 +1,22 @@
+package com.personal.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AboutServlet extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		this.getServletContext().getRequestDispatcher("/jsp/about.jsp").forward(req, resp);
+		
+		
+	}
+}
