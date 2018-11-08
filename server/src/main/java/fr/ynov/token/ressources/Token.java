3aa9d95cc4672d4fca38604c31dc226cc7381e6e
LudovicRobez <ludovic.robez@hotmail.fr>
2018-11-08 15:25:27 +0100

    Mise à jour du pom.xml

---
+++ server/src/main/java/fr/ynov/token/ressources/Token.java
@@ -0,0 +1,14 @@
+package fr.ynov.token.ressources;
+
+public class Token {
+
+    private String token;
+
+    public Token(String login, String password) {
+        token = "";
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
