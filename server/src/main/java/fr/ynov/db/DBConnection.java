ec7e78c0978493b9e23a95919c2e6653c278f840
LudovicRobez <ludovic.robez@hotmail.fr>
2018-11-08 15:11:42 +0100

    Ajout du Singleton pour la connection a la DB

--- server/src/main/java/fr/ynov/db/DBConnection.java
+++ server/src/main/java/fr/ynov/db/DBConnection.java
@@ -1,4 +1,42 @@
 package fr.ynov.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+
+    public final static String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+    /**
+     * Instance property : Connection
+     */
+    private static Connection instance;
+
+    /**
+     * Url property : String, (parameters added for Timezone issues between driver and server database)
+     */
+
+    public final static String url = "jdbc:mysql://localhost:3306/5g?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    /**
+     * User property : String
+     */
+    public final static String user = "root";
+
+    /**
+     * Password property : String
+     */
+    public final static String password = "root";
+
+    /**
+     * Method which returns a connection whether from instance property or from the constructor if null
+     * @return instance property : Connection
+     */
+    public static Connection getConnection() throws SQLException,ClassNotFoundException {
+        if(instance == null) {
+            Class.forName(DBConnection.jdbcDriver);
+            instance = DriverManager.getConnection(url, user, password);
+        }
+        return instance;
+    }
 }
